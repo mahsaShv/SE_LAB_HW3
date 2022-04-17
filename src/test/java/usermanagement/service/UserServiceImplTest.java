64b20a113cf7d83d92c58ec6cc28eb4f76e88252
mahsaShv <Msheikhi@ce.sharif.edu>
2022-04-17 06:14:46 +0430

    Reports and tests are finalized

--- src/test/java/usermanagement/service/UserServiceImplTest.java
+++ src/test/java/usermanagement/service/UserServiceImplTest.java
@@ -1,7 +1,24 @@
 package usermanagement.service;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import usermanagement.repository.PersonRepository;
 
 public class UserServiceImplTest extends TestCase {
+    @Test
+    public void test_save_and_find() {
+        User user1 = new User();
+        User user2 = new User();
+        user1.setUserId(1);
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.save(user1);
+        userService.save(user2);
+
+        assertEquals(userService.findById(1).equals(user1), true);
+
+
+    }
 
 }
--- src/test/java/usermanagement/service/UserTest.java
+++ src/test/java/usermanagement/service/UserTest.java
@@ -1,7 +1,40 @@
 package usermanagement.service;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+import usermanagement.entity.Person;
+
+import static org.junit.Assert.assertEquals;
 
 public class UserTest extends TestCase {
+
+    @Test
+    public void test_user_equals() {
+        User user1 = new User();
+        User user2 = new User();
+
+        user2.setUserId(1);
+        user1.setUserId(1);
+        assertEquals(user1.equals(user2), true);
+
+        user2 = null;
+        assertEquals(user1.equals(user2), false);
+
+    }
+
+    @Test
+    public void test_user_setters_getters() {
+        User user1 = new User();
+        user1.setUserId(1);
+        user1.setFirstName("Mahsa");
+        user1.setLastName("Sheikhi");
+        user1.setCompanyName("myCompany");
+
+        assertEquals(user1.getUserId().intValue(), 1);
+        assertEquals(user1.getFirstName(), "Mahsa");
+        assertEquals(user1.getCompanyName(), "myCompany");
+        assertEquals(user1.getLastName(), "Sheikhi");
+    }
+
 
 }
