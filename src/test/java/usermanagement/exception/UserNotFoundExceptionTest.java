b4d8514707090d1f4b76b004b0099aecaf454e78
mahsaShv <Msheikhi@ce.sharif.edu>
2022-04-17 06:22:10 +0430

    added some tests

--- src/test/java/usermanagement/exception/UserNotFoundExceptionTest.java
+++ src/test/java/usermanagement/exception/UserNotFoundExceptionTest.java
@@ -1,7 +1,15 @@
 package usermanagement.exception;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
-public class UserNotFoundExceptionTest extends TestCase {
+import static org.junit.Assert.assertEquals;
+
+public class UserNotFoundExceptionTest {
+    @Test
+    public void userNotFoundExceptionTest() {
+        UserNotFoundException exception = new UserNotFoundException("message", 20);
+        assertEquals(20, exception.getUserId().intValue());
+    }
 
 }
--- src/test/java/usermanagement/service/TransformServiceTest.java
+++ src/test/java/usermanagement/service/TransformServiceTest.java
@@ -1,3 +1,6 @@
 package usermanagement.service;
 
-public class TransformServiceTest { }
+public class TransformServiceTest {
+
+    
+}
